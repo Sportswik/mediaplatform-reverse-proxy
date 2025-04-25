7e4948734e63acec33d2e15221a2926127922a96
sjolundmartin <martinsjolund@hotmail.com>
2025-04-25 16:07:01 +0200

    Added react routes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const REACT_APP_URL = "https://wa-admin-minfotboll-test.azurewebsites.net";
 const DOTNET_APP_URL = "https://mysoccertest.ontariosoccer.net";
 
 // First path segments that should go to the React app
-const reactPaths = ["users", "api", "_next", "static"];
+const reactPaths = ["users", "login", "verification", "api", "_next", "static"];
 
 // Middleware to route requests to appropriate target
 app.use("*", (req, res, next) => {
